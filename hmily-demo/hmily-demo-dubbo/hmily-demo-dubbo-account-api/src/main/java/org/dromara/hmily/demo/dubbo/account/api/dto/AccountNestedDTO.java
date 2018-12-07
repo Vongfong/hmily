158ca7eac02e3f7daa3120dc999e65e4bee60467
Vongfong <dsy3018@gmail.com>
2018-12-07 15:38:44 +0800

    修改对应配置及增加getter和setter方法

--- hmily-demo/hmily-demo-dubbo/hmily-demo-dubbo-account-api/src/main/java/org/dromara/hmily/demo/dubbo/account/api/dto/AccountNestedDTO.java
+++ hmily-demo/hmily-demo-dubbo/hmily-demo-dubbo-account-api/src/main/java/org/dromara/hmily/demo/dubbo/account/api/dto/AccountNestedDTO.java
@@ -51,5 +51,35 @@ public class AccountNestedDTO implements Serializable {
      */
     private Integer count;
 
+    public String getUserId() {
+        return userId;
+    }
 
+    public void setUserId(String userId) {
+        this.userId = userId;
+    }
+
+    public BigDecimal getAmount() {
+        return amount;
+    }
+
+    public void setAmount(BigDecimal amount) {
+        this.amount = amount;
+    }
+
+    public String getProductId() {
+        return productId;
+    }
+
+    public void setProductId(String productId) {
+        this.productId = productId;
+    }
+
+    public Integer getCount() {
+        return count;
+    }
+
+    public void setCount(Integer count) {
+        this.count = count;
+    }
 }
--- hmily-demo/hmily-demo-springcloud/hmily-demo-springcloud-order/src/main/java/org/dromara/hmily/demo/springcloud/order/dto/AccountDTO.java
+++ hmily-demo/hmily-demo-springcloud/hmily-demo-springcloud-order/src/main/java/org/dromara/hmily/demo/springcloud/order/dto/AccountDTO.java
@@ -42,4 +42,6 @@ public class AccountDTO implements Serializable {
     private BigDecimal amount;
 
 
+
+
 }
